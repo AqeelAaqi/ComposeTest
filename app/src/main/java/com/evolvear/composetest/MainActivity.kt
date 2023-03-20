9b10df1fd5ca028f50b018ed1b01e47bbe9c3ef0
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2023-03-20 18:47:05 +0500

    Toolbar in Jetpack Compose

--- app/src/main/java/com/evolvear/composetest/MainActivity.kt
+++ app/src/main/java/com/evolvear/composetest/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.evolvear.composetest
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,13 +25,54 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "JetPack Compose") },
+                            navigationIcon = {
+                                IconButton(onClick = { }) {
+                                    Icon(
+                                        Icons.Filled.Menu ,
+                                        contentDescription = "menu"
+                                    )
+                            } },
+                            actions = {
+                                IconButton(onClick = { }) {
+                                    Icon(
+                                        Icons.Filled.Notifications ,
+                                        contentDescription = "notifications"
+                                    )
+                                }
+                                IconButton(onClick = { }) {
+                                    Icon(
+                                        Icons.Filled.Search ,
+                                        contentDescription = "search"
+                                    )
+                                }
+                            }
+                        )
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {}) {
+                            IconButton(onClick = { }) {
+                                Icon(
+                                    Icons.Filled.Add ,
+                                    contentDescription = "add"
+                                )
+                            }
+                        }
+                    },
+                    floatingActionButtonPosition = FabPosition.End,
                 ) {
-                    Greeting("Android")
+                    Greeting(name = "Android")
                 }
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    Greeting("Android")
+//                }
             }
         }
     }
@@ -36,12 +81,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Column() {
-        Text(text = "Hello $name!", color = Color.Red, style = TextStyle(fontWeight = FontWeight.ExtraBold) , softWrap = true)
-        Text(text = "Aqeel $name!", color = Color.Red, style = TextStyle(fontWeight = FontWeight.ExtraBold) , softWrap = true)
+        Text(text = "Hello $name!", color = Color.Red)
+//        Text(text = "Aqeel $name!", color = Color.Red, style = TextStyle(fontWeight = FontWeight.ExtraBold) , softWrap = true)
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "light mode")
+@Preview(showBackground = true,  name = "dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
     ComposeTestTheme {
